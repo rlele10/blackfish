274ab88940cd99d545b45e2bc8c6c2a01baad605
Rasika <rasika@Rasikas-MacBook-Pro.local>
2020-11-09 17:01:35 -0700

    updted rpute

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import ResultsForm from './components/results/ResultsForm';
 import ResultsShow from './components/results/ResultShow';
 import OrcaLogo from './components/images/OrcaLogo';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 
 const App = () => (
   <Router>
     <OrcaLogo></OrcaLogo>
-    <Route  exact path="/" component={ResultsForm} />
+   
+    <Link to="/my-test-results">Go to results</Link>
+
     <Route  exact path="/my-test-results" component={ResultsForm} />
     <Route path="/view-result" component={ResultsShow} />
   </Router>
