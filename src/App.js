1deead003ced333c196d749979c5fdb62cf7e785
Logan Walker <logan@orcahealth.com>
2020-11-10 10:06:02 -0700

    lw - text: added disclaimer to form

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import ResultsForm from './components/results/ResultsForm';
 import ResultsShow from './components/results/ResultShow';
 import OrcaLogo from './components/images/OrcaLogo';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Disclaimer from './components/shared/Disclaimer';
 
 
 const App = () => (
-  <Router>
-    <OrcaLogo></OrcaLogo>
-    <Route  exact path="/" component={ResultsForm} />
-    <Route   exact path="/my-test-results" component={ResultsForm} />
-    <Route path="/view-result" component={ResultsShow} />
-  </Router>
+  <>
+    <Router>
+      <OrcaLogo></OrcaLogo>
+      <Route  exact path="/" component={ResultsForm} />
+      <Route   exact path="/my-test-results" component={ResultsForm} />
+      <Route path="/view-result" component={ResultsShow} />
+    </Router>
+    <Disclaimer></Disclaimer>
+  </>
 )
 
 export default App;
---
+++ src/components/shared/Disclaimer.js
@@ -0,0 +1,18 @@
+import { Container, Grid, Form} from 'semantic-ui-react';
+
+
+const Disclaimer = () => (
+
+  <Container>
+  <Grid centered textAlign='justified' >
+      <Grid.Row >
+          <Grid.Column width={12}>
+          <p>As a healthcare-technology company, we operate in accordance with all applicable privacy, data, and HIPAA protection laws.</p>
+          </Grid.Column>
+      </Grid.Row>
+  </Grid>
+</Container>
+
+)
+
+export default Disclaimer;
