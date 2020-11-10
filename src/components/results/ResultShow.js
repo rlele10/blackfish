82057c0091b5303297f301d8e7d4d4aa16710c01
Logan Walker <logan@orcahealth.com>
2020-11-10 15:49:08 -0700

    lw - styling: formate ResultShow page and included disclaimer only in ResultForm

--- src/components/results/ResultShow.js
+++ src/components/results/ResultShow.js
@@ -1,45 +1,50 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
-import {  Table } from 'semantic-ui-react'
+import {  Table, Container, BreadcrumbDivider } from 'semantic-ui-react'
+import styled from 'styled-components'
+import { TableContainer } from '../styles/Table'
+
+
+
 const ResultShow = props => {
   console.log(props)
  
-
     return (
-        <div>
-    <Table celled>
+      <>
+        <TableContainer>
+          <Table celled striped centered>
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell>Full Name</Table.Cell>
+                <Table.Cell>{props.location.state.Patient_First_Name}   {props.location.state.Patient_Last_Name}</Table.Cell>
+              </Table.Row>
+              <Table.Row >
+                <Table.Cell>Collection Date</Table.Cell>
+                <Table.Cell> {props.location.state.Collection_Date}  </Table.Cell>
+              </Table.Row>
+              <Table.Row >
+                <Table.Cell>Result Date</Table.Cell>
+                <Table.Cell> {props.location.state.Report_Date}  </Table.Cell>
+              </Table.Row>
+              <Table.Row >
+                <Table.Cell>Test Result</Table.Cell>
+                <Table.Cell> {props.location.state.Test_Result}  </Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table>
+        </TableContainer>
 
-    <Table.Body>
-      <Table.Row>
-        <Table.Cell>Full Name</Table.Cell>
-        <Table.Cell>{props.location.state.Patient_First_Name}   {props.location.state.Patient_Last_Name}</Table.Cell>
-       </Table.Row>
+        <br></br>
 
-      <Table.Row >
-        <Table.Cell>Collection Date</Table.Cell>
-        <Table.Cell> {props.location.state.Collection_Date}  </Table.Cell>
-      </Table.Row>
-
-      <Table.Row >
-        <Table.Cell>Result Date</Table.Cell>
-        <Table.Cell> {props.location.state.Report_Date}  </Table.Cell>
-      </Table.Row>
-
-
-      <Table.Row >
-        <Table.Cell>Test Result</Table.Cell>
-        <Table.Cell> {props.location.state.Test_Result}  </Table.Cell>
-      </Table.Row>
-
-    </Table.Body>
-  </Table>
-        <NavLink to="/" activeClassName="active">
-        Go Back
-      </NavLink>
-     
+        <div style={{textAlign: 'center'}}>
+          <NavLink to="/" activeClassName="active" style={{fontSize: '1.25em', color: '#0058A2'}}>
+          Go Back
+          </NavLink>
         </div>
+      </>
     
     );
 };
 
 export default ResultShow;
+
