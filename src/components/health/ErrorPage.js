88dde8234561e74b7b69013459ecfa8d99b460c1
Logan Walker <logan@orcahealth.com>
2020-12-04 17:13:36 -0700

    lw - text: redirect message change

--- src/components/health/ErrorPage.js
+++ src/components/health/ErrorPage.js
@@ -9,9 +9,44 @@ const ErrorPage = props => {
       <>
         <Container>
           <OrcaHealthLogo></OrcaHealthLogo>
-            <Header as='h3' textAlign='center'>
-              We are currently under construction. Sorry for the inconvenience. We will be back soon with some awesome features. 
+            <Header as='h4'>
+              We are excited to announce that we are currently refactoring our software platform, bringing improvements to the following key features: 
             </Header>
+              <ul>
+                <li> Patient Acquisition </li>
+                  <ul>
+                    <li> Find a Specialist, Provider Profile, Scheduling, etc. </li>
+                  </ul>
+                <li> Patient Check-Ups </li>
+                <ul>
+                  <li> NPI, PROMs, PSAT, PUNS, etc. </li>
+                </ul>
+                <li> Patient Education </li>
+                <ul>
+                  <li> Pre-Hab, Re-Hab, Physical Therapy, Home Health, etc. </li>
+                </ul>
+                <li> Post-Visit Care Pathways </li>
+                <ul>
+                  <li> Digital Biomarkers, Range of Motion, etc. </li>
+                </ul>
+                <li> Pre-Visit Care Pathways </li>
+                <ul>
+                  <li> Directional Videos, Patient Intake, etc. </li>
+                </ul>
+                <li> Reputation Enhancement Campaigns </li>
+                <ul>
+                  <li> Google Reviews, Healthgrades, Orca, Yelp, etc. </li>
+                </ul>
+              </ul>
+
+            <Header as='h4'>
+              Qualitatively, the new system is designed to dramatically improve your patient’s experience, education, engagement, and outcomes. Quantitatively, the new system is designed to dramatically improve treatment uptake, automated feedback loops, and financial outcomes.
+            </Header>
+
+            <Header as='h4'>
+              Join the waiting list for beta access <a href="https://orcahealth.typeform.com/to/bBIkfiaD#source=xxxxx">here.</a>
+            </Header>
+            
         </Container>
       </>
     );
