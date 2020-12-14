059c9febd9a9bd1aa51e96f4a18586ec2ee3031d
Logan Walker <ltw@Logans-MacBook-Pro.local>
2020-12-14 10:35:13 -0700

    lw - pages: home page with links to patient and provider intake

---
+++ src/pages/Home.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import { Header } from 'semantic-ui-react';
+import OrcaLogo from '../components/images/OrcaLogo';
+import { SubmitButton } from '../components/styles/Button';
+
+
+const Home= props => {
+  console.log(props)
+  
+    return (
+      <>
+      <OrcaLogo></OrcaLogo>
+
+          <Header as='h2' textAlign='center'> COVID-19 Testing </Header>
+
+          <Header as='h3' textAlign='center'> Are you a </Header>
+
+          <div style={{textAlign: 'center'}}>
+            <NavLink to="/patient-intake">
+              <SubmitButton>
+                Patient
+              </SubmitButton>
+            </NavLink>
+          </div>
+
+          <Header as='h3' textAlign='center'> or</Header>
+
+          <div style={{textAlign: 'center'}}>
+            <NavLink to="/provider-intake">
+              <SubmitButton>
+                Provider
+              </SubmitButton>
+            </NavLink>
+          </div>
+
+          <br></br>
+      </>
+    
+    );
+};
+
+export default Home;
