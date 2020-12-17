ac1d64fdacb645d5b65581aee9d3b7c8332abf1a
Logan Walker <ltw@Logans-MacBook-Pro.local>
2020-12-17 12:48:30 -0700

    lw - styling: changed button size

--- src/components/styles/Button.js
+++ src/components/styles/Button.js
@@ -6,35 +6,36 @@ export const SubmitButton = styled.button`
   border-radius: 4px;
   border: 2.5px solid black;
   padding: .25em;
+  margin-top: 1em;
 
   @media (min-width: 320px) and (max-width: 480px) {
     width: 8em;
     height: auto;
-    font-size: .9em;
+    font-size: 1.25em;
   }
 
   @media (min-width: 481px) and (max-width: 767px) {
     width: 12em;
     height: auto;
-    font-size: 1em;
+    font-size: 1.4em;
   }
 
   @media (min-width: 768px) and (max-width: 1024px) {
     width: 12em;
     height: auto;
-    font-size: 1.25em;
+    font-size: 1.5em;
   }
 
   @media (min-width: 1025px) and (max-width: 1280px) {
     width: 14em;
     height: auto;
-    font-size: 1.25em;
+    font-size: 1.6em;
   }
 
   @media (min-width: 1281px) {
     width: 14em;
     height: auto;
-    font-size: 1.5em;
+    font-size: 1.6em;
   }
 
 
